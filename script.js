8fa06892c63aa588b5db95a24c5f30e8514e5757
saber <1999ms@live.fr>
2025-03-14 17:08:54 +0100

    fix style

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.dropdown');
+    const dropdownContent = document.querySelector('.dropdown-container');
+
+    dropdown.addEventListener('click', function () {
+        if (dropdownContent.style.display === 'flex') {
+            dropdownContent.style.display = 'none';
+        } else {
+            dropdownContent.style.display = 'flex';
+        }
+    });
+
+    // Close the dropdown if clicked outside
+    document.addEventListener('click', function (event) {
+        if (!dropdown.contains(event.target)) {
+            dropdownContent.style.display = 'none';
+        }
+    });
+});
